c272d757ea322199706204b9a17c7c941259f9cd
ManojTaleka <manoj89biet@gmail.com>
2016-09-30 11:49:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 
 var element = document.getElementById("text-main");
 element.innerHTML = 'New content coming';
+
+var img = document.getElementById("madi");
+img.onclick = function(){
+img.style.marginLeft = '100px';
+    
+};
