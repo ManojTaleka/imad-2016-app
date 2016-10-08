9e80934f3561254d24fb482758d85ae1483eb44d
ManojTaleka <manoj89biet@gmail.com>
2016-10-08 19:29:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,8 @@
-console.log('Loaded!');
-
-
-var element = document.getElementById("text-main");
-element.innerHTML = 'New content coming';
-
-var img = document.getElementById("madi");
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-    
-}
-
-img.onclick = function(){
-var interval = setInterval(moveRight,100);
-    
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function()
+{
+  counter = counter +1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
