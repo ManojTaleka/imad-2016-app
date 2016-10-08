f29cce44fa1c85d97c138e641a95685eead8d07d
ManojTaleka <manoj89biet@gmail.com>
2016-10-08 20:06:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,6 +16,6 @@ button.onclick = function()
           }
       }
   };
-  request.open('GET','http://manojtaleka.imad.hasura-app.io/counter',true);
+  request.open("GET","http://manojtaleka.imad.hasura-app.io/counter",true);
   request.send(null);
 };
